cd884acc651915fca0be6bf5310453f8bf34154b
talana tefo <talanatefo@gmail.com>
2025-03-22 15:49:32 +0200

    added navbar

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import Navbar from "./Navbar";
 function HomePage() {
   return (
     <>
-      <div className="h-screen bg-gradient-to-b from-[#204725] to-[#2A1010]">
-        <h1>HomePage</h1>
+      <div className="h-screen bg-gradient-to-b from-[#204725] to-[#2A1010] text-white">
+        <Navbar />
       </div>
     </>
   );
