eacfc7a3f177f126324db990ad46cc53d7b87da9
talana tefo <talanatefo@gmail.com>
2025-03-23 08:30:20 +0200

    updated search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 function Searchbar() {
   return (
     <>
-      <div className="bg-white w-2/5 rounded-full mx-auto text-gray-500  text-center">
-        <input type="text" placeholder="Search any movie " />
+      <div className="bg-white w-2/5 rounded-full mx-auto text-gray-500  text-center py-2 ">
+        <input
+          className="outline-none w-100"
+          type="text"
+          placeholder="Search any movie "
+        />
       </div>
     </>
   );
