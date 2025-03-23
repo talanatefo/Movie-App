751a9d5b4dc77da00ce83d1c8aacdc261db28fdc
talana tefo <talanatefo@gmail.com>
2025-03-23 19:28:34 +0200

    added search icon

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MovieCard from "./MovieCard";
+
 function MoviesGrid() {
   return (
     <div className="grid grid-cols-6 grid-rows-2 gap-4 h-92 mx-20 mt-5 ">
